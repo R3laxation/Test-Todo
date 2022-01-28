9d54c04e4dedc7ad58090c4120436aa87adcddfb
R3laxation <memories@bk.ru>
2022-01-28 19:39:09 +0600

    Changed Project.tsx

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import styles from './Projects.module.scss';
 import {Input} from "../../components/input/Input";
 import {Button} from "../../components/button/Button";
