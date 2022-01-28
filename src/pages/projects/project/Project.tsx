afbf27f586ba028b1b63900f16ef2996cc28fc07
R3laxation <memories@bk.ru>
2022-01-28 19:28:44 +0600

    Changed Project.tsx

--- src/pages/projects/project/Project.tsx
+++ src/pages/projects/project/Project.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import styles from './Project.module.scss';
+import {useAppSelector} from "../../../bll/slices/store";
+import {selectProjects} from "../../../bll/slices/projects/selectors";
+import {ProjectType} from "../../../bll/slices/projects/types";
 
 export const Project = () => {
 
+    const projects = useAppSelector<ProjectType[]>(selectProjects);
 
+    const mappedProjects = projects.map(({id, title, addedDate}) => (
+        <Link to={`/project/${id}`} className={styles.project}>
+            <h3>{title}</h3>
+            <h4>{addedDate}</h4>
+        </Link>
+    ))
 
     return (
-        <Link to={`/project/`} className={styles.project}>
-            <h3>Title</h3>
-            <h4>Added Date</h4>
-        </Link>
+        {mappedProjects}
     );
 };
 
