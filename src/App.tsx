dbfbece50986a6364ba404e5c43ba19cab5c345b
R3laxation <memories@bk.ru>
2022-11-24 20:13:28 +0600

    Changed App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import { Projects } from './pages/projects/Projects';
-
+import { Tasks } from './pages/tasks/Tasks';
 
 function App() {
   return (
     <Routes>
         <Route path={'/'} element={<Projects/>}/>
-        <Route />
+        <Route path={'project/:id'} element={<Tasks/>}/>
     </Routes>
   );
 }
