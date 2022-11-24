8f7504e18b7729a429a0ac4f7258b8137fc3e50f
R3laxation <memories@bk.ru>
2022-11-24 19:34:34 +0600

    Created tasksSlice.ts

---
+++ src/bll/slices/tasks/tasksSlice.ts
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+export const tasksSlice = createSlice({
+    name: 'projects',
+    initialState: [],
+    reducers: {
+
+    },
+    extraReducers:{}
+});
+
+export default tasksSlice.reducer;
