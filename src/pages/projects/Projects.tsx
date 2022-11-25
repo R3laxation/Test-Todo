18fa125dc845deba9cca5d9513a35816666b16be
R3laxation <memories@bk.ru>
2022-11-25 19:04:26 +0600

    Changed Projects styles

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import styles from './Projects.module.scss';
 import {Input} from "../../components/input/Input";
+import {Button} from "../../components/button/Button";
 
 export const Projects = () => {
     return (
-            <div>
-                <Input/>
+            <div className={styles.projects}>
+                <div className={styles.addForm}>
+                    <Input/>
+                    <Button>Add Project</Button>
+                </div>
                 <Link to={`/project/`} className={styles.project}>
                     <h3>Title</h3>
                     <h4>Added Date</h4>
