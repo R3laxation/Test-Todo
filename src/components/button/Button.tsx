2d05b086d2d2c808d9789977bfed16fae3236e5a
R3laxation <memories@bk.ru>
2022-11-26 18:39:01 +0600

    Filled Button.module.scss

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {ButtonPropsType} from "./types";
+import styles from './Button.module.scss';
 
 export const Button: React.FC<ButtonPropsType> = (
     {
@@ -8,9 +9,11 @@ export const Button: React.FC<ButtonPropsType> = (
     }
 ) => {
 
+    const finalClassName = `${red ? styles.red : styles.default}  ${light ? styles.light : styles.default} ${className}`
+
     return (
         <button
-
+            className={finalClassName}
             style={{width}}
             {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
         />
