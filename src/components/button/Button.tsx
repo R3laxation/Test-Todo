0dbfc74b9503abffb907704dae18d8032250bc5d
R3laxation <memories@bk.ru>
2022-11-26 18:35:18 +0600

    Filled Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {ButtonPropsType} from "./types";
 
-export const Button: React.FC<ButtonPropsType> = () => {
-    return (
-        <div>
+export const Button: React.FC<ButtonPropsType> = (
+    {
+        red, light, className, width,
+        ...restProps
+    }
+) => {
 
-        </div>
+    return (
+        <button
+
+            style={{width}}
+            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+        />
     );
 };
 
