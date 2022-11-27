cabd0db35e093dd6f6de5f88f089f124caf42cca
R3laxation <memories@bk.ru>
2022-11-27 17:21:22 +0600

    Added Header.module.scss

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.scss';
 
 export const Header = () => {
     return (
-        <div>
+        <div className={style.header}>
             <h1>Daily projects</h1>
         </div>
     );
