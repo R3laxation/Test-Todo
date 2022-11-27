32b0b4ac6d76c00ac2bfcc8e1bcd30d3c14f2403
R3laxation <memories@bk.ru>
2022-11-27 16:43:36 +0600

    Changed App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import { Projects } from './pages/projects/Projects';
-import { Tasks } from './pages/tasks/Tasks';
+import {Projects} from './pages/projects/Projects';
+import {Tasks} from './pages/tasks/Tasks';
+import {MainLayout} from "./layouts/MainLayout";
 
 function App() {
-  return (
-    <Routes>
-        <Route path={'/'} element={<Projects/>}/>
-        <Route path={'project/:id'} element={<Tasks/>}/>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={''} element={<Projects/>}/>
+                <Route path={'project/:id'} element={<Tasks/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
