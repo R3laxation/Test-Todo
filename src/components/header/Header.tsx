8ed16cf021d267fb80d2f3a8e29d053511a0c157
R3laxation <memories@bk.ru>
2022-11-27 16:30:17 +0600

    Created MainLayout.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <div>
-
+            <h1>Daily projects</h1>
         </div>
     );
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Header} from "../components/header/Header";
+import {Outlet} from "react-router-dom";
 
 export const MainLayout = () => {
     return (
         <>
-
+            <Header/>
+            <div>
+                <Outlet/>
+            </div>
         </>
     );
 };
